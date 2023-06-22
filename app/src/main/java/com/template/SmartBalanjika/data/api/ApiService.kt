fe121c44646334acd7496ea96e868f41e8dba411
sindhu Rammohan <sindhusargunam@gmail.com>
2023-06-22 22:28:47 +0200

    update readme file

--- app/src/main/java/com/template/SmartBalanjika/data/api/ApiService.kt
+++ app/src/main/java/com/template/SmartBalanjika/data/api/ApiService.kt
@@ -18,13 +18,13 @@ interface ApiService {
 
 //    @GET(ApiEndPoints.PHOTOS_URL)
     //change with correct API
-    @GET("https://raw.githubusercontent.com/SindhuRammohan/Smartbalanjika/main/README.md")
+    @GET("https://raw.githubusercontent.com/SindhuRammohan/Smartbalanjika/main/app/src/main/res/API/apicall.txt")
     suspend fun getPhotosUrl(): Response<List<Photos>>
 
 //    @GET(ApiEndPoints.PDF_URL)
-    @GET("https://raw.githubusercontent.com/SindhuRammohan/Smartbalanjika/main/README.md")
+    @GET("https://raw.githubusercontent.com/SindhuRammohan/Smartbalanjika/main/app/src/main/res/API/apicall.txt")
     suspend fun getPdfsUrl(): Response<List<pdf>>
 
-    @GET("https://raw.githubusercontent.com/SindhuRammohan/Smartbalanjika/main/README.md")
+    @GET("https://raw.githubusercontent.com/SindhuRammohan/Smartbalanjika/main/app/src/main/res/API/apicall.txt")
     suspend fun getSangamList(): Response<List<Sangam>>
 }
